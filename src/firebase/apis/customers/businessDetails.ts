b2134e11b59156ebf1fbd154780efd8c13fe2097
Daniel Olagunju <todak2000@gmail.com>
2023-06-03 04:24:57 +0100

    feat(postApis): update apis and other post related apis done

---
+++ src/firebase/apis/customers/businessDetails.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { collection, getDocs, query, where } from '@firebase/firestore';
+
+import { db } from '@/firebase/FirebaseStore';
+import { getWeekday, isWorkingHours, timeConverter } from '@/utils';
+import { vendorReturnDataProps } from '@/utils/types';
+
+// Get user details
+const VendorFormDetailsApi = async (
+  formLinkID: string
+): Promise<vendorReturnDataProps | void> => {
+  try {
+    const vendorDetails = collection(db, 'Vendors');
+
+    const vendorQuery = query(
+      vendorDetails,
+      where('formLink', '==', formLinkID)
+    );
+    const querySnapshot = await getDocs(vendorQuery);
+    const weekday = getWeekday(new Date());
+    if (querySnapshot.docs.length > 0) {
+      const vendorData: any = {
+        phoneNumber: querySnapshot.docs[0].data().phoneNumber,
+        businessName: querySnapshot.docs[0].data().businessName,
+        isOperating:
+          querySnapshot.docs[0].data().currentOperationStatus['operation'],
+        isWorkingDay: querySnapshot.docs[0].data().workingDays[0][weekday],
+        isWorkHours: isWorkingHours(
+          timeConverter(querySnapshot.docs[0].data().openingHour),
+          timeConverter(querySnapshot.docs[0].data().closingHour),
+          new Date()
+        ),
+      };
+      return {
+        status: 200,
+        message: 'vendor data retrieved successful',
+        vendorData,
+      };
+    }
+  } catch (err: any) {
+    return {
+      status: err.status,
+      message: err.message,
+    };
+  }
+};
+export default VendorFormDetailsApi;
---
+++ src/firebase/apis/customers/queueList.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { collection, getDocs, query, where } from '@firebase/firestore';
+
+import { db } from '@/firebase/FirebaseStore';
+import { dateFormaterString } from '@/utils';
+import { queueLinkReturnDataProps, queueListDataProps } from '@/utils/types';
+// Get user details
+
+const QueueListApi = async (
+  formLinkID: string
+): Promise<queueLinkReturnDataProps | void> => {
+  const queueArray: queueListDataProps[] = [];
+  const data: queueListDataProps[] = [];
+  let sortedArray: queueListDataProps[] = [];
+
+  const today = dateFormaterString(new Date().toString());
+  try {
+    const vendorDetails = collection(db, 'Vendors');
+
+    const vendorQuery = query(
+      vendorDetails,
+      where('formLink', '==', formLinkID)
+    );
+    const querySnapshot = await getDocs(vendorQuery);
+    if (querySnapshot.docs.length > 0) {
+      const uid = querySnapshot.docs[0].id;
+      const queueDB = collection(db, `Form-${uid}`);
+
+      const queueQuery = query(queueDB, where('status', '<=', 1));
+      const queueSnapshot = await getDocs(queueQuery);
+
+      if (queueSnapshot.docs.length > 0) {
+        queueSnapshot.docs.map((doc) => {
+          queueArray.push({
+            date: dateFormaterString(doc?.data().date),
+            ticketNo: doc?.data().ticketNo,
+            name: doc?.data().name,
+            queueNumber: doc?.data().queueNumber,
+          });
+        });
+        sortedArray = queueArray.sort((a, b) => a.queueNumber - b.queueNumber);
+
+        sortedArray
+          .filter((doc) => doc.date === today)
+          .map((d) => {
+            data.push(d);
+          });
+      }
+
+      return {
+        status: 200,
+        message: 'queue List data retrieved successful',
+        queueArray: data,
+        totalQueue: data.length,
+      };
+    }
+  } catch (err: any) {
+    return {
+      status: err.status,
+      message: err.message,
+    };
+  }
+};
+export default QueueListApi;
